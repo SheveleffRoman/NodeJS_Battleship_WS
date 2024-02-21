c242d1971f49d3e6ac7119bed1119f08fcf8cea1
SheveleffRoman <@>
2024-02-21 17:22:58 +0400

    feat: add ws server with main handler

--- src/ws/ws-server.ts
+++ src/ws/ws-server.ts
@@ -1,11 +1,18 @@
-import { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from "ws";
+import dotenv from "dotenv";
+import { handleClientRequest } from "../handlers/clientRequestHandler.js";
 
-const wsPort = 3000;
+dotenv.config();
+
+const ws_port = Number(process.env.WS_PORT) || 3000;
 
 export const wsServer = new WebSocketServer({
-  port: wsPort,
+  port: ws_port,
 });
 
-wsServer.on('connection', () => {
-  console.log('Connected with WebSocket');
+wsServer.on("connection", (ws: WebSocket) => {
+  console.log("Connected with WebSocket");
+  ws.on("message", (message: WebSocket.RawData) => {
+    handleClientRequest(message, ws);
+  });
 });
